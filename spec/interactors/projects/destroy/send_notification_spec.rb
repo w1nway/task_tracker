160fd629fa7ce53751503cdfb42c00d4bb7ebfaf
w1nway <lauren_gg@mail.ru>
2022-11-16 21:03:08 +0300

    send_notification_spec folders created, .rspec modified

---
+++ spec/interactors/projects/destroy/send_notification_spec.rb
@@ -0,0 +1,26 @@
+require "rails_helper"
+
+describe Projects::Destroy::SendNotification do
+  include ActiveJob::TestHelper 
+
+  describe ".call" do 
+    context "with correct params" do 
+      let(:interactor) { described_class.new(project: project) }
+      let(:project) { create :project }
+
+      before do
+        allow(ProjectMailer).to receive(:project_destroyed).and_call_original
+      end
+
+      after do
+        clear_enqueued_jobs
+      end 
+
+      it "sends mail" do
+        expect(ProjectMailer).to receive(:project_destroyed)
+
+        expect { interactor.run }.to change(enqueued_jobs, :size).by(1)
+      end
+    end
+  end
+end
---
+++ spec/interactors/projects/update/send_notification_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+describe Projects::Update::SendNotification do
+  include ActiveJob::TestHelper
+
+  describe ".call" do
+    context "with correct params" do 
+      let(:interactor) { described_class.new(project: project) }
+      let(:project) { create :project }
+
+      before do 
+        allow(ProjectMailer).to receive(:project_updated).and_call_original
+      end
+
+      after do 
+        clear_enqueued_jobs
+      end 
+      
+      it "sends mail" do 
+        expect(ProjectMailer).to receive(:project_updated)
+
+        expect { interactor.run }.to change(enqueued_jobs, :size).by(1)
+      end
+    end
+  end
+end
+
