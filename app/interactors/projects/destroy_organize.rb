c8de7796ba71106e67b854ea388237073642bc4f
MaximStadnikov <73811696+MaximStadnikov@users.noreply.github.com>
2022-11-17 14:51:06 +0300

    fix interactors (#53)

--- app/interactors/projects/destroy_organize.rb
+++
@@ -1,8 +0,0 @@
-module Projects
-  class Destroy
-    include Interactor::Organizer
-
-    organize Projects::Destroy::Execute,
-             Projects::Destroy::SendNotification
-  end
-end
--- app/interactors/projects/update.rb
+++ app/interactors/projects/update.rb
@@ -1,13 +1,8 @@
 module Projects
   class Update
-    class Execute
-      include Interactor
+    include Interactor::Organizer
 
-      delegate :project, :project_params, to: :context
-
-      def call
-        context.fail!(error: "Invalid Data") unless project.update(project_params)
-      end
-    end
+    organize Projects::Update::Execute,
+             Projects::Update::SendNotification
   end
 end
