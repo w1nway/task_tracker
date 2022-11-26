a45230cc0088c576eb11e16115bb506df3ed9381
w1nway <lauren_gg@mail.ru>
2022-11-26 20:53:41 +0300

    hw10 with additional task realized

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -28,17 +28,27 @@ class ProjectsController < ApplicationController
 
   # GET /projects/1/edit
   def edit
+    respond_to do |format|
+      format.html { render :edit }
+      format.json do
+        render json: { project: @project }
+      end
+    end
   end
 
   # POST /projects
-  def create
+  def create # rubocop:disable Metrics/MethodLength
     @project = create_project.project
 
     if create_project.success?
-      redirect_to @project, notice: "Project was successfully created."
+      respond_to do |format|
+        format.html { redirect_to @project, notice: "Project was successfully created." }
+        format.json do
+          render json: { project: @project, errors: @errors }
+        end
+      end
     else
-      flash.now[:alert] = "Something went wrong. Try again."
-      render :new
+      render json: { project: @project, errors: @project.errors }
     end
   end
 
