7c7d4c08d4493ac43eebd4085386d5e2e0959dda
w1nway <lauren_gg@mail.ru>
2022-11-27 21:46:07 +0300

    WIP

--- app/mailers/task_mailer.rb
+++ app/mailers/task_mailer.rb
@@ -13,6 +13,7 @@ class TaskMailer < ApplicationMailer
 
     for user in project.users do
       if user.role == 'member'
+        @member = user
         mail(to: member.email.pluck(:email))
       end
     end
@@ -25,10 +26,30 @@ class TaskMailer < ApplicationMailer
     mail(to: project.users.pluck(:email))
   end
 
-  def task_destroyed(project, task)
+  def task_destroyed_by_user(project, task, user)
     @project = project 
     @task = task
 
-    mail(to: project.users.pluck(:email))
+    mail(to: user.email)
+  end
+
+  def task_destroyed_by_owner(project, task)
+    @project = project 
+    @task = task 
+    @owner = ProjectMembership.find_by(project: @project, role: :owner).user
+
+    mail(to: owner.email.pluck(:email))
+  end
+
+  def task_destroyed_notification(project, task, current_user)
+    @project = project
+    @task = task
+    
+    for user in project.users do
+      if user.role == 'member' && user != current_user
+        @member = user
+        mail(to: member.email.pluck(:email))
+      end
+    end
   end
 end
