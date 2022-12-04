7c5b3b5b5b3a99b9bad63e886cc79eb4959f3a8f
w1nway <lauren_gg@mail.ru>
2022-12-04 22:41:57 +0300

    removed redundant payload field for destroy project mutation

--- app/graphql/types/payloads/destroy_project_payload.rb
+++ app/graphql/types/payloads/destroy_project_payload.rb
@@ -2,7 +2,6 @@ module Types
   module Payloads
     class DestroyProjectPayload < Types::BaseObject
       field :project, ProjectType, null: true
-      field :errors, [Types::UserError], null: false
     end
   end
 end
