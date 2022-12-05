7883394e70f9f8f8c75fba7261d7922fa18d4691
w1nway <lauren_gg@mail.ru>
2022-12-05 21:09:39 +0300

    added current_user field in payload for mutation

--- app/graphql/types/payloads/sign_in_payload.rb
+++ app/graphql/types/payloads/sign_in_payload.rb
@@ -1,6 +1,7 @@
 module Types
   module Payloads
     class SignInPayload < Types::BaseObject
+      field :current_user, CurrentUserType, null: false, method: :user
       field :access_token, String, null: true
       field :errors, [Types::UserError], null: true
     end
