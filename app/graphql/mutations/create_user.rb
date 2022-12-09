a38be1ca34d4e683da590e28c6197c3268c69802
w1nway <lauren_gg@mail.ru>
2022-12-09 10:49:08 +0300

    resolved conversations

--- app/graphql/mutations/create_user.rb
+++ app/graphql/mutations/create_user.rb
@@ -1,7 +1,5 @@
 module Mutations
   class CreateUser < BaseMutation
-    include AuthenticableGraphqlUser
-
     argument :input, Types::Inputs::CreateUserInput, required: true
 
     type Types::Payloads::CreateUserPayload
@@ -9,11 +7,7 @@ module Mutations
     def resolve(input:)
       result = Users::Create.call(user_params: input.to_h)
 
-      if result.success?
-        result.to_h
-      else
-        result.to_h.merge(errors: formatted_errors(result.user))
-      end
+      result.to_h.merge(errors: formatted_errors(result.user))
     end
   end
 end
--- app/graphql/types/payloads/create_user_payload.rb
+++ app/graphql/types/payloads/create_user_payload.rb
@@ -1,7 +1,7 @@
 module Types
   module Payloads
     class CreateUserPayload < Types::BaseObject
-      field :user, UserType, null: true
+      field :user, UserType, null: false
       field :errors, [Types::UserError], null: false
     end
   end
