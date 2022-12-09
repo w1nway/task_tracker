77d8db029e1b052ff5bd970bf1f67ff612bba5fa
w1nway <lauren_gg@mail.ru>
2022-12-09 09:24:57 +0300

    resolved conversations

--- app/graphql/types/payloads/destroy_project_payload.rb
+++ app/graphql/types/payloads/destroy_project_payload.rb
@@ -1,7 +1,8 @@
 module Types
   module Payloads
     class DestroyProjectPayload < Types::BaseObject
-      field :project, ProjectType, null: true
+      field :project, ProjectType, null: false
+      field :errors, [Types::UserError], null: true
     end
   end
 end
