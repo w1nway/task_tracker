3b8af197fd7b7919639d2893351e395d2fc20dbb
w1nway <lauren_gg@mail.ru>
2022-12-10 16:40:26 +0300

    fixed type of user_role

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -6,6 +6,6 @@ module Types
     field :email, String
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
-    field :user_role, Enum, null: false
+    field :user_role, UserRoleEnum, null: false
   end
 end
