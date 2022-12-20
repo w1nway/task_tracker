56b989b6d5482ab78b82645e77bffe9bd502a06a
w1nway <lauren_gg@mail.ru>
2022-12-20 11:27:55 +0300

    new homework

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   end
 
   resources :users, only: %i[new create]
-  resource :sessions, only: %i[new create show]
+  resource :sessions, only: %i[new create show destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
