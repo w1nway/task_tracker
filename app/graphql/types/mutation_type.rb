f1ad3fa311e1e27e28d5a464c3941cfad3a31739
AndreyBartnovskiy <andrey.bartnovskiy@stackers.team>
2023-04-24 15:48:21 +0300

    for no conflicts change

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -4,9 +4,7 @@ module Types
     field :sign_in, mutation: Mutations::SignIn
 
     field :create_project, mutation: Mutations::CreateProject
-    
     field :update_project, mutation: Mutations::UpdateProject
-    
     field :destroy_project, mutation: Mutations::DestroyProject
 
     field :create_task, mutation: Mutations::CreateTask
